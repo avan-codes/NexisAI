f28292b8f34c9883b35a2f80df73c8e0734c5079
avan-codes <157613147+avan-codes@users.noreply.github.com>
2026-09-16 11:49:24 +0530

    Delete frontend directory

--- frontend/src/api/commands.js
+++
@@ -1,4 +0,0 @@
-import client from './client';
-
-export const createCommand = (repo_id, text) => client.post('/commands', { repo_id, text });
-export const getCommands = (params) => client.get('/commands', { params });
--- frontend/src/api/executions.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const getExecutions = (params) => client.get('/executions', { params });
-export const getExecution = (id) => client.get(`/executions/${id}`);
-export const approveExecution = (id) => client.post(`/executions/${id}/approve`);
-export const rejectExecution = (id, reason) => client.post(`/executions/${id}/reject`, { reason });
-export const retryExecution = (id) => client.post(`/executions/${id}/retry`);
